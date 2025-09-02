93692484319af0e572bc537d89e3c9f2da9e3fc2
andrescheffer28 <anddrecosttascheffer@gmail.com>
2025-09-02 15:24:23 -0300

    add: handler e server

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"encurtador-urls/api"
+	"log/slog"
+	"net/http"
+	"time"
+)
 
+func main() {
+	if err := run(); err != nil {
+		slog.Error("failed to execute code", "error", err)
+		return
+	}
+	slog.Info("all systems offline")
+}
+
+func run() error {
+	handler := api.NewHandler()
+
+	s := http.Server{
+		Addr:         ":8080",
+		Handler:      handler,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  time.Minute,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
 }
